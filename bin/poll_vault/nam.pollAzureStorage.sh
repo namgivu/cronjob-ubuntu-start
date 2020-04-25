1dbae08d218d4ed2a1dc315573176272f75ec08e
namgivu <namgivu@gmail.com>
2020-04-25 19:58:59 +0700

    cannot use pipenv with cronjob so run directly from .venv/bin/python

--- bin/poll_vault/nam.pollAzureStorage.sh
+++ bin/poll_vault/nam.pollAzureStorage.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 SH=$(cd `dirname $BASH_SOURCE` && pwd)  # SH aka SCRIPT_HOME the containing folder of this script
-AH=$(cd "$SH/.." && pwd)  # AH aka APP_HOME
-
-LOG="$AH/log/log.txt"
+AH=$(cd "$SH/../.." && pwd)  # AH aka APP_HOME
 
 cd $AH
-    PYTHONPATH=$AH  pipenv run python "./src/main.py"  | tee -a $LOG
-    #                                 app entry point        log file
+    PYTHONPATH=$AH  ./.venv/bin/python ./src/main.py
+    #                                  app entry point
 cd - 1>/dev/null
+
+#TODO move nam.pollAzureStorage .sh script and py app into folder ./bin/poll_vault/nam.pollAzureStorage/
