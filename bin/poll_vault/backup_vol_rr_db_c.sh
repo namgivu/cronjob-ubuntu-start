677917ed18ef830012128d9c1649fdcbf169a53b
minh_quan <quanhk711@gmail.com>
2021-03-19 23:51:57 +0700

    clear

--- bin/poll_vault/backup_vol_rr_db_c.sh
+++ bin/poll_vault/backup_vol_rr_db_c.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-docstring='
-BACKUP_FROM_C="db_C" ./backup_vol_postgres.sh
-'
+set -aux
+BACKUP_FROM_C=rr_db_c
 
 SH=$(cd `dirname $BASH_SOURCE` && pwd)
-AH=$(cd "$SH/.." && pwd)
 log_f="$SH/.backup/log"
 
 [ -z $BACKUP_FROM_C ] && (echo 'Envvar BACKUP_FROM_C is required' ; kill $$)  # name container's volume you want backup
-
 
 TIME=`date +%Y%m%d_%H%M%S`
 FILE_TAR="backup-$TIME.tar"
@@ -20,14 +17,14 @@ FILE_TAR="backup-$TIME.tar"
     docker run -dit \
         --name $BACKUP_MIDDLEMAN_C      `# we use a :ubuntu ctn to tar folder :DEFAULT_POSGRES_DATA_DIR in ctn :BACKUP_FROM_C ` \
         --volumes-from $BACKUP_FROM_C   `# apply volume in ctn :BACKUP_FROM_C into this :backup_middleman ctn ie it will have postgres data mounted at @backup_middleman:$DEFAULT_POSGRES_DATA_DIR` \
-        -v "$BACKUP_TO_DIR:/backup"  `# backup file to be stored here` \
+        -v "$BACKUP_TO_DIR:/backup:rw"  `# backup file to be stored here` \
         ubuntu
 
         # do backup
         #TODO review do we need -i?
-        docker exec -i $BACKUP_MIDDLEMAN_C /bin/bash -c "cd $DEFAULT_POSGRES_DATA_DIR && tar cvf /backup/$FILE_TAR ." | tee $log_f
+        docker exec -i $BACKUP_MIDDLEMAN_C /bin/bash -c "cd $DEFAULT_POSGRES_DATA_DIR && tar cvf /backup/$FILE_TAR ." >> $log_f
 
-        docker rm -f $BACKUP_MIDDLEMAN_C  # clean up
+        docker rm -f $BACKUP_MIDDLEMAN_C  > /dev/null 2>&1 # clean up
 
     # backup aftermath check
     backup_f="$BACKUP_TO_DIR/$FILE_TAR"
